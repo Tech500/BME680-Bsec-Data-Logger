96b99d14c225b3bbceb4fbe7b98a38268dd71ec5
William Lucid <11734452+Tech500@users.noreply.github.com>
2021-03-09 10:58:01 -0500

    Update index3.h

--- index3.h
+++ index3.h
@@ -12,12 +12,12 @@ const char HTML3[] PROGMEM = R"====(
 	<h2>BME680 Observations</h2>
 	<br>
   <frameset rows="30%,70%" cols="33%,34%">
-  <iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/1320323/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=BME680+Temperature&type=line&xaxis=Date+and+Time&yaxis=Temperature+F."></iframe>
-  <iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/1320323/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=BME680+Humidity&type=line&xaxis=Date+and+Time&yaxis=Humidity"></iframe>
+  //Two "Thingspeak.com" Iframes go here...
+	
   </frameset>
   <br><br>
-  <frameset rows="30%,70%" cols="33%,34%">
-  <iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/1320323/charts/3?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=BME680+Pressure&type=line&xaxis=Date+and+Time&yaxis=Pressure+inHg."></iframe>
+  //Two "Thingspeak.com" Iframes go here...
+ 
   <iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/1320323/charts/4?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=BME680+Gas&type=line&xaxis=Date+and+Time&yaxis=Gas+Res++KOhms"></iframe>
   </frameset>
  	<br><br>
